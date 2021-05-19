aaeb04cae901c1c518994779589f16f961702218
Francisco <fnaveira11@gmail.com>
2021-05-18 20:51:08 -0400

    commit tree 3dbb20b09fd0e9f6e742f94351bb3ab9f1413df5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,16 +27,7 @@ class String
   end
 
   def count_sentences
-    self.split('.').count
-    # binding.pry
-   
-    # self.each do |word|
-      
-    #   if
-    #     word.end_with?(".")
-    #   end
-    # end
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 
 end
